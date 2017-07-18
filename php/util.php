9aa5c936cc2b96ec0fb2dbab16b6fd36bf478b5b
liupo <liupo.wu@wz-inc.com>
2017-07-18 10:44:16 +0800

    remove google libs; renmae php to static html

--- php/util.php
+++ php/util.php
@@ -1,13 +1,9 @@
 <?php
 	/**
 	 * Determines if we are running on the live server (or locally)
-	 * 
+	 *
 	 */
 	function isLive() {
-		if(preg_match('/experiments.dev|localhost/', $_SERVER['SERVER_NAME'])) {
-			return false;
-		} else {
-			return true;
-		}
+		return !preg_match('/experiments.dev|localhost/', $_SERVER['SERVER_NAME']);
 	}
 ?>
